bd365a737053fd16e472c386f0875287b7c2ac36
mike140 <micky_1408sands@hotmail.com>
2016-04-30 15:49:07 -0500

    Main menu principal

--- main.pl
+++ main.pl
@@ -1,48 +1,99 @@
 #!/usr/bin/perl
 
-sub readData; # se avisa al programa que mas adelante se declara esta función
-
 use strict;
 use warnings;
+use Switch;
 
-open LOGIN, "< login.txt" or die "No pudo realizarse la conexion con la Base de Datos: $!\n";
-#se abre el archivo con los nombres de usuario y contraseñas
+#esto es para importar las funciones que estan en otro archivo
+use Database qw(getRegister writeFile getKeys getNextId fetchArray modif columReg);
 
-our %user; # hash que contendra todos los datos importantes del usuario
-my $registrado; #boleano para saber si el nombre de usuario esta registrado
-my $acceso; #boleano para ver si puede acceder, osea si la contraseña es correcta
-my @users = <LOGIN>; #se leen todas las lineas del archivo de usuarios y se guardan en un arreglo
+our $user; 
+my ($exit, $op) = (0); 
 
 do{
-    $registrado = 0; #como se van a pedir los datos registrado es falso
-	$user{name}= readData("\n\n\tBIBLIOTECA\nIngrese nombre de Usuario: ");
-	$user{pass} = readData("\nIngrese password: ");
-	system("cls"); 
+    print "\tBIBLIOTECA\n\n";
+    $op = menu( {1 => "Ingresar", 2 => "Buscar Libro", 3 => "About", 4 => "Salir"} );
 
-	for (@users){ #se recorre el arreglo de lineas del archivo buscando el nombre del usuario
-		if(/(.*),$user{name},(.*)/){ #expr regular que busca el nombre de usuario $1 = tipo,  $2 = password
-			$registrado = 1; 
-			$acceso = $user{pass} eq $2; #se compara la contraseña dada por el usuario con la que está en el archivo ($2)
-			$user{type} = $1; #el tipo del usuario se guarda en $1
-			print( ("Pass incorrecto", "Bienvenido al sistema", )[$acceso] );
-			last; #se sale del for
-		}
-	}
+    switch($op){
+        case 1 {
+            my $userName = readData("Ingrese nombre de usuario: ");
+            my $pass = readData("Ingrese password: ");
+            $user = fetchArray("login.txt", "([^,]+,){2}$userName,$pass,[^,]+,"); 
 
-	if(!$registrado){
-		print "El usuario proporcionado no esta registrado";
-	}
-}while(!$acceso);
+            if($user && $user->{STATUS} eq "1"){
+                pause("Bienvenido $user->{nombre}!)";
+            }
+            else{
+                pause("Error, usuario o password incorrecto");
+            }
+        }
+        
+        case 2{
+            #ID,name,author,year,genero,editorial,cantidad,status,sinopsis
+            my $op = menu( {1 => "Por id", 2 => "Por status", 3 => "Por nombre", 4 => "genero", 5 => "Libre"} );
+        }
 
-system "cls";
-print "\nBienvenido!\nEl tipo de usuario es $user{type}";
+        case 3 { pause("Informacion del sistema"); }
+        
+        case 4 { 
+            my $ans = readData("Seguro que deseas salir? y/n\nR: ");
+            $exit = 1 if ($ans eq "y");
+        }
+    }
+    system("cls");
+}while(!$exit);
 
-sub readData{
-	print $_[0]; #imprime el argumento dado a la función
-	my $value = <STDIN>;  #lee una linea del usuario
-	chomp $value; #le quita el \n
+sub readData{ #imprime un mensaje (shift) , pide un dato al usuario, le quita el \n y lo devuelve
+	print shift; 
+	chomp(my $value = <STDIN>); 
 	return $value; 
 }
+
+sub pause{ #imprime un mensaje y una pausa
+    print $_[0], "\n";
+    system "pause";
+}
+
+sub menu{ #recibe un mapa para imprimir un menu, verifica la opcion que este bien y la devuelve
+    my ($map, $message, $validInput, $op) = (shift, "", 0, 0);
+    my @options = ();
+
+    foreach (sort { $a <=> $b } keys %{$map}){
+        $message .= $_ . ". " . $map->{$_} . "\n";
+        push @options, $_;
+    } 
+    $message .= "\n\nR: ";
+
+    do{
+       print $message;
+       chomp ($op = <STDIN>);
+
+       $validInput = grep(/^$op$/, @options);
+       if(!$validInput){
+            pause "Porfavor, escoga una opcion del menu";
+            system "cls";
+       }
+    }while(!$validInput);
+
+    system "cls";
+    return $op;
+}
+
+sub getTime{ #obtiene tiempo y fecha y lo devuelve como un mapa
+    gmtime() =~ /(\S+) (\S+) (\S+) (\d\d:\d\d)\S+ (\S+)/;
+    #Sat Feb 16 13:50:45 2013
+    return ( day => $1, month => $2, daynumber => $3, hour => $4, year => $5);
+}
+
+sub printMap{ #recorre un mapa y lo imprime
+    my $map = shift;
+
+    foreach (sort keys %{$map}){
+        print $_, ". ", $map->{$_}, "\n";
+    }
+}
+
+
 
 
 
